119fd228716a23787706288a3456836e85467fec
Alexiy1984 <alevaliev@yandex.ru>
2020-10-09 21:30:12 +0300

    added form and part of input/js for modal window

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -1,6 +1,23 @@
 window.addEventListener('load', function () {
   var modal_open = document.querySelectorAll('[data-toggle=modal]');
   var modal_close = document.getElementsByClassName('modal-close');
+  var password_show = document.querySelectorAll('.password-toggler');
+
+  var getSiblings = function (elem) {
+    var siblings = [];
+    var sibling = elem.parentNode.firstChild;
+
+    while (sibling) {
+      if (sibling.nodeType === 1 && sibling !== elem) {
+        siblings.push(sibling);
+      }
+
+      sibling = sibling.nextSibling;
+    }
+
+    return siblings;
+  };
+
   [].forEach.call(modal_open, function (btn) {
     btn.onclick = function () {
       var modals = document.querySelectorAll('.modal');
@@ -17,6 +34,18 @@ window.addEventListener('load', function () {
   [].forEach.call(modal_close, function (btn) {
     btn.onclick = function (event) {
       event.target.closest('.modal').style.display = 'none';
+    };
+  });
+  [].forEach.call(password_show, function (trigger) {
+    trigger.onclick = function (event) {
+      var inputs = getSiblings(event.target),
+          input = inputs[0];
+
+      if (input.type === 'password') {
+        input.type = 'text';
+      } else {
+        input.type = 'password';
+      }
     };
   });
 
